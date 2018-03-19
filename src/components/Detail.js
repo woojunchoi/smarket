b0ac01a5dd5c0682edb260a6749b3b243045425e
woojunchoi <tim.woojune1@gmail.com>
2018-03-19 01:29:35 -0700

    now rendering detail page info

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import '../App.css';
+import moment from 'moment'
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
 
 class Detail extends Component {
   constructor(props) {
@@ -12,21 +15,38 @@ class Detail extends Component {
 
   componentDidMount() {
     const param = this.props.match.params.eventId
-    axios.get(`https://cors-anywhere.herokuapp.com/https://fe-api.smarkets.com/v0/events/id/${param}`)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://fe-api.smarkets.com/v0/events/id/${param}`,
+      {cancelToken: source.token}
+    )
     .then(res => this.setState({
       detail:res.data
     }))
   }
 
+  componentWillUnmount() {
+    source.cancel('unmounted');
+  }
+  // (example: name, league, start time)
+
   render() {
     console.log(this.state.detail)
+
+
     if(this.state.detail) {
       return (
-        <div>
-        <p>
-          {this.state.detail.name}
-        </p>
-        </div>
+        <div className ='detail-box'>
+        <div className="row center-align">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <h3>{this.state.detail.event.name}</h3>
+              <h4>{this.state.detail.event.event_type} League</h4>
+            </div>
+            <div className="card-content white-text">
+            <h4>{this.state.detail.event.start_datetime}</h4>
+            </div>
+          </div>
+      </div>
+      </div>
       )
     }
     return (
