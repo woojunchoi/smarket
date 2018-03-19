bd7b5e696c0ed3d22277ad2e8e9f4a8479984c24
woojunchoi <tim.woojune1@gmail.com>
2018-03-19 07:39:21 -0700

    added smarket website theme and updated readme

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -18,19 +18,21 @@ class Detail extends Component {
   }
 
   render() {
+    console.log(this.props.detail)
     //if state is not null, dom manipulate with data from API
     if(this.props.detail !== null) {
       return (
         <div className ='detail-box'>
           <div className="row center-align">
-            <div className="card blue-grey darken-1">
+            <div className="card">
               <div className="card-content white-text">
-                <h4>{this.props.detail.event.name}</h4>
-                <h5>League : {this.props.detail.event.event_type}</h5>
+                <h5>{this.props.detail.event.name}</h5>
+                <h6>topic : {this.props.detail.event.event_type}</h6>
+                <h6>league : {this.props.detail.event.full_slug.split('/')[3]}</h6>
               </div>
               <div className="card-content white-text">
-                <h5 style={{color:'black'}}><Moment format="MMMM Do YYYY hh:mm a">{this.props.detail.event.start_datetime}</Moment></h5>
-                <h5 style={{color:'black'}}><Moment fromNow>{this.props.detail.event.start_datetime}</Moment></h5>
+                <h6 style={{color:'#8c8c8c'}}><Moment format="MMMM Do YYYY hh:mm a">{this.props.detail.event.start_datetime}</Moment></h6>
+                <p style={{color:'#8c8c8c'}}><Moment fromNow>{this.props.detail.event.start_datetime}</Moment></p>
               </div>
             </div>
         </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {  Link } from "react-router-dom";
-
+import '../css/List.css'
 class List extends Component {
 
   render() {
