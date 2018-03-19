63cb54bb338547ccfff06c03eff7e88af171f07b
woojunchoi <tim.woojune1@gmail.com>
2018-03-18 23:25:35 -0700

    componentwillunmount is added for performance

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Link } from "react-router-dom";
 import axios from 'axios'
 import Detail from './components/Detail'
 import List from './components/List'
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -13,15 +16,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://cors-anywhere.herokuapp.com/https://fe-api.smarkets.com/v0/events/popular/')
+    axios.get('https://cors-anywhere.herokuapp.com/https://fe-api.smarkets.com/v0/events/popular/',
+    {cancelToken: source.token}
+    )
     .then(res => this.setState({
       events:res.data.results
     })
     )}
-
+  
+  componentWillUnmount() {
+    source.cancel();
+  }
 
   render() {
-
+    console.log(this.state.events)
 
     return (
       <div className="container">
